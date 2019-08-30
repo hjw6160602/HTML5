291855d58de2a236545958a7b736e7a3d9da446f
贺嘉炜 <hejiawei@hejiaweideMacBook-Pro.local>
2019-08-30 17:35:05 +0800

    add vue modules and vue-router

--- HelloVue/js/index.js
+++ HelloVue/js/index.js
@@ -9,20 +9,50 @@ var musicData = [
     {id:4, name:"Russ-Psycho(Pt.4)", auth:"Russ", songSrc:'./music/Russ-Psycho(Pt.4).mp3'},
 ];
 
+Vue.component('Test', {
+    data(){
+        return {
+            msg:'Hello world!'
+        }
+    },
+    template:
+        '<div>\
+            <div>{{msg}}</div> \
+        </div>',
+
+    beforeCreated:function(){
+        console.log(this);
+    },
+    created:function(){
+        console.log(this.msg);
+    }
+})
+
 new Vue({
     el:'#app',
     data(){
         return{
-            musicData:musicData
+            musicData:musicData,
+            currentIndex:0
         }
     },
+
     computed:{
+        // 计算属性默认只有getter
         getCurrentSongSrc:function(){
-            return this.musicData[2].songSrc
+            set:function(newV) {
+                console.log(newV);
+            }
+            get(newV){
+
+            }
+            return this.musicData[this.currentIndex].songSrc
         }
     },
     methods:{
-
+        clickHandler(index){
+            this.currentIndex = index;
+        }
     }
 })
 
